ad4ac4b3abe61d0f1fc12f5f7ba42221fc782a78
Miguel Vega Perez <miguelvegap10@gmail.com>
2025-06-18 17:41:29 -0500

    added login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { EyeAlertFrontLayoutComponent } from './eyealert-front/layouts/eye-alert
 
 export const routes: Routes = [
     {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.routes')
+    },
+    {
         path: '',
         loadChildren: () => import('./eyealert-front/eyealert-front.routes')
     }

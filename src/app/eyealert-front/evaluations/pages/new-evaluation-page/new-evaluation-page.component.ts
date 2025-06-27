ca973cc87dea8be378d26ec84ab978af49182441
Miguel Vega Perez <aries_242015@hotmail.com>
2025-06-27 18:41:19 -0500

    added design new evaluation page

--- src/app/eyealert-front/evaluations/pages/new-evaluation-page/new-evaluation-page.component.ts
+++ src/app/eyealert-front/evaluations/pages/new-evaluation-page/new-evaluation-page.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'new-evaluation-page',
   imports: [],
-  templateUrl: './new-evaluation-page.component.html'
+  templateUrl: './new-evaluation-page.component.html',
+  styleUrl: './new-evaluation-page.component.css'
 })
 export class NewEvaluationPageComponent {
 

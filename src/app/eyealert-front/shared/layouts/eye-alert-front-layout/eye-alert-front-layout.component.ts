38375dfb40bd14dd3f7c965880a21e8b2c1be6b5
Miguel Vega Perez <aries_242015@hotmail.com>
2025-07-01 03:10:17 -0500

    added design responsive on new evaluation page

--- src/app/eyealert-front/shared/layouts/eye-alert-front-layout/eye-alert-front-layout.component.ts
+++ src/app/eyealert-front/shared/layouts/eye-alert-front-layout/eye-alert-front-layout.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { SideBarComponent } from '../../components/side-bar/side-bar.component';
 import { SideBarService } from '../../components/side-bar/side-bar.service';
 
 @Component({
   selector: 'app-eye-alert-front-layout',
-  imports: [RouterOutlet, SideBarComponent],
+  imports: [RouterOutlet, SideBarComponent, RouterLink],
   templateUrl: './eye-alert-front-layout.component.html',
   styleUrl: './eye-alert-layout.component.css'
 })

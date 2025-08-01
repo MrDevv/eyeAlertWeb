640be8582f6cf3981b4eaad1d837e186ca431102
Miguel Vega Perez <aries_242015@hotmail.com>
2025-07-31 20:07:21 -0500

    added plural pipe in card-user-score component

--- src/app/eyealert-front/home/components/card-user-score/card-user-score.component.ts
+++ src/app/eyealert-front/home/components/card-user-score/card-user-score.component.ts
@@ -1,16 +1,22 @@
-import { UpperCasePipe } from '@angular/common';
-import { Component, input, ResourceRef } from '@angular/core';
+import { I18nPluralPipe, UpperCasePipe } from '@angular/common';
+import { Component, input, ResourceRef, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { UserDTO } from '@auth/interfaces/UserDTO';
 
 @Component({
   selector: 'card-user-score',
-  imports: [UpperCasePipe, RouterLink],
+  imports: [UpperCasePipe, RouterLink, I18nPluralPipe],
   templateUrl: './card-user-score.component.html',  
   styleUrl: './card-user-score.component.css'
 })
 export class CardUserScoreComponent {
   user = input.required<UserDTO | null>()
   scoreUser = input.required<ResourceRef<any | undefined>>()
+
+  pointsMap = signal({
+    "=1": "# punto",
+    other: "# puntos"
+  })
+
 }
